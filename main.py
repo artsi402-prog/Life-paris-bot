8146ba4c69a75fa6ed17d5df8ef260bcfd54465c
artsi402-prog <arthursimon402@gmail.com>
2026-02-08 23:07:36 +0100

    Initialize Discord bot with basic setup
    Ww

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot connecté : {bot.user}")
+
+TOKEN = os.getenv("TOKEN")
+bot.run(TOKEN)
